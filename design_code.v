dfa3f177557a772a98c9a21175f9e8b4717975b5
arpitshuklalsg01-ops <arpitshuklalsg01@gmail.com>
2026-05-05 14:10:16 +0530

    design_code.v
    # single-port RAM with Synchronous read/write and asynchronous reset

---
+++ design_code.v
@@ -0,0 +1,26 @@
+module teamFSM (
+    input clk,
+    input rst,    
+    input wr_en,      
+    input [1:0]  addr,     
+    input [3:0]  wdata,    
+    output reg  [3:0]  rdata 
+);
+  reg [3:0] R_mem [0:3];
+    integer i;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+          for (i = 0; i < 4; i = i + 1)begin
+              R_mem[i] <= 4'b0000;
+          end
+            rdata <= 4'b0000;
+        end
+      else if (we)begin
+            mem[addr] <= wdata;
+      end
+        else begin
+            rdata <= mem[addr];
+        end
+    end
+endmodule
